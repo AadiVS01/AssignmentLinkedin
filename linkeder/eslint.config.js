71e82fa6c38fbd816c8a1df428f18b62bae63f11
Aadi Vinayak .S <81699764+evilrudeandharsh@users.noreply.github.com>
2025-08-03 14:00:15 +0530

    linter

--- linkeder/eslint.config.js
+++ linkeder/eslint.config.js
@@ -16,10 +16,17 @@ export default tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
+      
     ],
     rules: {
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/prefer-optional-chain": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         { prefer: "type-imports", fixStyle: "inline-type-imports" },
